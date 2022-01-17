13790dda1e54333d652244da0373f7a1f546195c
erdsaesdrse <97901220+erdsaesdrse@users.noreply.github.com>
2022-01-17 22:35:39 +0700

    Update bored.sh

--- bored.sh
+++ bored.sh
@@ -4,3 +4,8 @@ library(RCurl)
 options(RCurlOptions = list(proxy = "socks5h://178.154.220.124:1080"))
 my.handle <- getCurlHandle()
 html <- getURL(url='https://www.torproject.org', curl=my.handle)
+curl ipinfo.io; echo
+if ! hash ping &>/dev/null; then
+  echo "Installing ping tools ..."
+  apt-get install iputils-ping -y &>/dev/null
+fi
